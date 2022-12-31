296dc30dcf1b9878f5fe89dd32d5ad8c06b3ab28
chatethakhun <chatethakhun.jack@gmail.com>
2022-12-31 07:41:46 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/pokemons', (req, res) => {
+  const data = [];
+
+  fs.createReadStream("./pokemonData.csv")
+    .pipe(
+      parse({
+        delimiter: ",",
+        columns: true,
+        ltrim: true,
+      })
+    )
+    .on("data", function (row) {
+      // 👇 push the object row into the array
+      data.push(row);
+    })
+    .on("error", function (error) {
+      console.log(error.message);
+    })
+    .on("end", function () {
+      // 👇 log the result array
+      res.json(data)
+    });
+})
+
+app.listen(port, () => {
+  console.log("Starting node.js at port " + port);
+}); 

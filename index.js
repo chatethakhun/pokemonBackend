66e0ba2403bc2077620e24245aaf8d94626aea68
chatethakhun <chatethakhun.jack@gmail.com>
2022-12-31 07:49:45 +0700

    add root path

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const { parse } = require("csv-parse");
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.get('/', (req, res) => {
+  res.json({ message: "Hello Word"})
+})
 app.get('/pokemons', (req, res) => {
   const data = [];
 
